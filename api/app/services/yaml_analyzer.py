4230a1a5dfe8ba24760fbe08248ea9fa6ac8f0e8
Vinícius Olímpio <viniolimpio3@gmail.com>
2025-04-12 21:17:15 -0300

    Changing to reasoner model & Setting up docker-compose

--- api/app/services/yaml_analyzer.py
+++ api/app/services/yaml_analyzer.py
@@ -9,13 +9,13 @@ def analyze(yaml_content):
     system_context = "You are an Agent that helps identify misconfigurations and issues in Kubernetes YAML files. You will return a valid JSON with a list of issues found, ordered by severity. You will return a JSON with this spec: " + json.dumps(schema)
 
     response = client.chat.completions.create(
-        model="deepseek-chat",
+        model="deepseek-reasoner",
         messages=[
             {"role": "system", "content": system_context },
             {"role": "user", "content": yaml_content}
         ],
         stream=False
     )
-
+    # Remover o "json" e "```" do final da resposta
     response.choices[0].message.content = json.loads(response.choices[0].message.content.strip('`').strip().removeprefix('json').strip())
     return response
